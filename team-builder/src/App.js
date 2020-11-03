89dae892221a3bfc5821457976b33726c87e6585
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2020-11-02 18:24:42 -0700

    met MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,53 +2,39 @@ import React, { useState } from 'react';
 import './App.css';
 import Form from './Form';
 
-
-const initalTeamList = [
-  {
-    name: 'Danielle Koduru',
-    email: 'danielleKoduru@teams.com',
-    role: 'Front-End Developer',
-  },
-  {
-    name: 'Clare Jackson',
-    email: 'clarejackson@teams.com',
-    role: 'Front-End Developer',
-  },
-  {
-    name: 'Bradley Dion',
-    email: 'badleydion@teams.com',
-    role: 'Backend-End Developer',
-  },
-]
-
-const initialFormValues = {
-  name: '',
-  email: '',
-  role: '',
-};
-
+const teamList = [{
+  name: 'Danielle Koduru',
+  email: 'daniellekoduru@teams.com',
+  role: 'Front-End Developer',
+},
+{
+  name: 'Clare Jackson',
+  email: 'clarejackson@teams.com',
+  role: 'Front-End Developer',
+},
+{
+  name: 'Bradley Dion',
+  email: 'badleydion@teams.com',
+  role: 'Backend-End Developer',
+}]
 
 function App() {
-  const [teamMembers, setTeamMembers] = useState([]);
 
-  const [formValues, setFormValues] = useState(initialFormValues);
-
+  const [teamMembers, setTeamMembers] = useState(teamList)
 
   return (
     <div className="App">
-      <header>
-        <h1>Team Builder Form</h1>
-      </header>
-      <Form />
-      {initalTeamList.map((teamMembers, index) => {
-        return ([<h2 key={index}>{teamMembers.name}</h2>,
-        <p key={index}>{teamMembers.role}</p>,
-        <p key={index}>contact info: {teamMembers.email}</p>
-        ])
+      <h1>Team Builder Form</h1>
+      <Form teamMembers={teamMembers} setTeamMembers={setTeamMembers} />
+
+      {teamMembers.map((teamMember, index) => {
+        return <><h2 key={index}>{teamMember.name}</h2>
+          <p>{teamMember.role}</p>
+          <p>contact info: {teamMember.email}</p></>
       })}
     </div>
   );
-};
+}
 
 
 export default App;
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = (props) => {
+    const [teamMates, setTeamMates] = useState({
+        name: '',
+        email: '',
+        role: ''
+    });
+
+    const { teamMembers, setTeamMembers } = props
+
+    //Handle Changes
+    const handleChanges = (event) => {
+        setTeamMates({ ...teamMates, [event.target.name]: event.target.value })
+    }
+
+    const submitForm = (event) => {
+        event.preventDefault()
+        const newTeamMember = {
+            id: Date.now(),
+            name: teamMates.name,
+            role: teamMates.role,
+            email: teamMates.email
+        }
+        setTeamMembers([...teamMembers, newTeamMember])
+        setTeamMates({
+            name: '',
+            email: '',
+            role: ''
+        })
+    }
 
     return (
-        <form>
+        <form onSubmit={submitForm} >
+
             <label htmlFor="name">Name</label>
             <input
                 id="name"
                 type="name"
                 name="name"
                 placeholder="Enter Name"
-            // onChange={handleChanges}
-            // required
+                value={teamMates.name}
+                onChange={handleChanges}
+                required
             />
 
             <label htmlFor="email">Email</label>
@@ -20,8 +50,9 @@ const Form = (props) => {
                 type="name"
                 name="email"
                 placeholder="Enter Email"
-            // onChange={handleChanges}
-            // required
+                value={teamMates.email}
+                onChange={handleChanges}
+                required
             />
 
             <label htmlFor="role">Role</label>
@@ -30,11 +61,21 @@ const Form = (props) => {
                 type="name"
                 name="role"
                 placeholder="Enter Role"
-            // onChange={handleChanges}
-            // required
+                value={teamMates.role}
+                onChange={handleChanges}
+                required
             />
 
-            <button>Submit</button>
+            {/* <select>
+                <option value="">Select A Role</option>
+                <option value="Front-End">Front-End Developer</option>
+                <option value="Back-End">Back-End Developer</option>
+                <option value="UX/UI">UX/UI Designer</option>
+                <option value="DataScience">Data Scientist</option>
+                <option value="iOS">iOS Developer</option>
+            </select> */}
+
+            <button type="submit">Submit</button>
         </form>
     )
 }
